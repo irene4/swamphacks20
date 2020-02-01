e0adaea5d78a0e70add383367965f32f1f5311b1
ignalb <baxter.ignalb@gmail.com>
2020-02-01 04:07:43 -0500

    Incorporated functionality

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,17 +1,28 @@
 from flask import Flask, flash, redirect, render_template, request
-from random import randint
- 
-app = Flask(__name__)
- 
-@app.route("/")
-def index():
-    return render_template('home.html')
+from color import Color
+from dmc_type import DmcType
+import dmc_file
 
-@app.route('/process', methods=['POST'])
-def process():
-    rgbval = request.form.get("rgbval")
-    hexval = request.form.get("hexval")
-    return render_template("home.html", rgb=rgbval, hex=hexval)
+app = Flask(__name__)
+
+dmcs = DmcType()
+dmc_file.load('static/dmc_refs.csv', dmcs)
+
+def format(close : list) -> list:
+    out_list = []
+    for i in close:
+        out_list.append((i[0], dmcs.get(i[0])[0], dmcs.get(i[0])[1].asHex()))
+    return out_list
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        color_in = request.form["color_in"]
+    else:
+        color_in = "#000000"
+    target = Color(color_in)
+    closests = format(dmcs.getClosest(target, 15))
+    return render_template('home.html', results=closests)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
